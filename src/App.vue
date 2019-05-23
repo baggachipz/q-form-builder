6a4d33aa27477e8334cc2049b0c992e3051b1d62
Matt Curtis <matt.curtis@open-xchange.com>
2019-05-23 14:22:29 -0400

    Added rendering example

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,20 @@
 
     <q-page-container>
       <q-page>
+
+        <!-- form building -->
         <q-form-builder v-model="fields" />
+
+        <q-separator />
+
+        <!-- form rendering -->
+        <div class="q-pa-md q-form-container">
+          <h4>Form Rendering</h4>
+          <div v-for="(field, index) in fields" :key="index">
+            <component v-model="fieldData[field.cid]" @input="onInput" v-bind:is="getElement(field)" :label="field.label" :required="field.required" :field_options="field.field_options" :id="field.cid" :cid="field.cid" :ref="field.cid" debounce="500" />
+          </div>
+        </div>
+
       </q-page>
     </q-page-container>
   </q-layout>
@@ -20,24 +33,66 @@
 <script>
 
 import QFormBuilder from '@/components/QFormBuilder'
+import { TextElement, ParagraphElement, CheckboxesElement, RadioElement, DateElement, TimeElement, DropdownElement, EmailElement, NameElement, SimpleNameElement, AddressElement, PhoneElement, FileElement, PaymentElement, PageBreakElement, SectionBreakElement } from '@/components/index'
 
 export default {
   name: 'SampleApp',
   components: {
-    QFormBuilder
+    QFormBuilder,
+    TextElement,
+    ParagraphElement,
+    CheckboxesElement,
+    RadioElement,
+    DateElement,
+    TimeElement,
+    DropdownElement,
+    EmailElement,
+    NameElement,
+    SimpleNameElement,
+    AddressElement,
+    PhoneElement,
+    FileElement,
+    PaymentElement,
+    PageBreakElement,
+    SectionBreakElement
   },
   data: function () {
     return {
-      fields: []
+      fields: [],
+      fieldData: []
+    }
+  },
+  methods: {
+    /**
+     * When a value is input into the rendered form, echo it to the debug line
+     */
+    onInput (val, id) {
+      console.debug(`${id}: ` + JSON.stringify(val))
+    },
+    /**
+     * Determine the name of the Element object based on the 'field_type' of the field data object
+     */
+    getElement (field) {
+      let nameParts = field.field_type.split('_')
+      for (let i = 0; i < nameParts.length; i++) {
+        nameParts[i] = nameParts[i].charAt(0).toUpperCase() + nameParts[i].slice(1)
+      }
+      return nameParts.join('') + 'Element'
     }
   },
   watch: {
-      fields: {
-        handler(val) {
-          console.debug(val);
-        },
-        deep: true
-      }
+    fields: {
+      handler (val) {
+        console.debug(val)
+      },
+      deep: true
     }
+  }
 }
 </script>
+
+<style lang="stylus">
+  .q-form-container
+    max-width 820px
+    margin auto
+</style>
